18a78ec1de07ed05b8dc7153efb1603bb5b6b425
Paulo Rosado <paulohenrique110803@gmail.com>
2024-07-01 19:23:49 -0300

    adding check cards

--- script.js
+++ script.js
@@ -8,6 +8,16 @@ var audio = new Audio('audio.mp3');
 
 console.log(number)
 
+const cards = document.querySelectorAll('.card');
+
+cards.forEach(card => {
+    card.addEventListener('click', function() {
+        const cardId = parseInt(card.id); 
+        game(cardId);
+    });
+});
+
+
 function game(num) {
     if (num == number) {
         let bomb = document.getElementById(String(number))
@@ -15,16 +25,24 @@ function game(num) {
         bomb.style.backgroundImage = "url('https://img.odcdn.com.br/wp-content/uploads/2023/07/explosao-nuclear-1.png')"
         
         explosion.style.display = "block"
-
+        
         setTimeout(function() {
-            died.style.display = "block"
+            died.style.opacity = "1";
+            died.style.pointerEvents = "auto"
         }, 500);
 
-        setTimeout(function() {
-            died.style.display = "block"
-            explosion.style.display = "none"
+        setTimeout(function() { 
+            explosion.style.display = "none";
         }, 1700);
 
         audio.play();
+    } else {
+        let element = document.getElementById(String(num))
+        
+        element.style.backgroundColor = "green"
+        element.style.backgroundImage = "none"
+        element.innerHTML += "<img src='https://upload.wikimedia.org/wikipedia/commons/thumb/c/c2/Black_check.svg/800px-Black_check.svg.png' style='width: 50px; position'>"
+        element.style.pointerEvents = "none"
+
     }
 }
